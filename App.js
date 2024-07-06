74cd7b3d4fee77aaf788565380c9b7a4309138b6
RAHUL THAKUR <iamrahulthakur.1992@gmail.com>
2024-07-06 16:50:11 +0530

    Storing token in AsyncStorage

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import { useContext } from 'react';
+import { useState, useContext, useEffect, useCallback } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { StatusBar } from 'expo-status-bar';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+//import * as SplashScreen from 'expo-splash-screen';
 
 import LoginScreen from './screens/LoginScreen';
 import SignupScreen from './screens/SignupScreen';
@@ -9,6 +11,9 @@ import WelcomeScreen from './screens/WelcomeScreen';
 import { Colors } from './constants/styles';
 import AuthContextProvider, { AuthContext } from './store/auth-context';
 import IconButton from './components/ui/IconButton';
+
+// Keep the splash screen visible while we fetch resources
+//SplashScreen.preventAutoHideAsync();
 
 const Stack = createNativeStackNavigator();
 
@@ -66,12 +71,37 @@ function Navigation() {
   );
 }
 
+function Root() {
+  //const [isTryingLogin, setIsTryingLogin] = useState(true);
+  const authCtx = useContext(AuthContext);
+
+  useEffect(() => {
+    async function fetchToken() {
+      const storedToken = await AsyncStorage.getItem('token');
+
+      if (storedToken) {
+        authCtx.authenticate(storedToken)
+      }
+
+      //setIsTryingLogin(false);
+    }
+
+    fetchToken();
+  }, [])
+
+  //if(isTryingLogin){
+  //  return null;
+  //}
+
+  return <Navigation />
+}
+
 export default function App() {
   return (
     <>
       <StatusBar style="light" />
       <AuthContextProvider>
-        <Navigation />
+        <Root />
       </AuthContextProvider>
     </>
   );
